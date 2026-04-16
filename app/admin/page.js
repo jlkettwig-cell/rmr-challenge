904dc2170a27f32853e937db6c52d89073b5231d
jlkettwig <jlkettwig@gmail.com>
2026-04-16 20:50:26 +0200

    fix redirect after login

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,9 +1,22 @@
 "use client";
 
-import { signInWithRedirect } from "firebase/auth";
+import { signInWithRedirect, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
 import { auth, provider } from "../../utils/firebase";
 
 export default function Admin() {
+
+  // 👉 NEU: prüft ob bereits eingeloggt
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("ALREADY LOGGED IN:", user.email);
+        window.location.href = "/";
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const login = async () => {
     console.log("LOGIN START");
