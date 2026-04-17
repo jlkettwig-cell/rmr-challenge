51731d86bfe151e2415daebf5ca6086acd22c635
jlkettwig <jlkettwig@gmail.com>
2026-04-17 19:34:18 +0200

    switch to popup login

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { signInWithRedirect, onAuthStateChanged } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import { useEffect } from "react";
 import { auth, provider } from "../../utils/firebase";
 
@@ -19,9 +19,15 @@ export default function Admin() {
   }, []);
 
   const login = async () => {
-    console.log("LOGIN START");
-    await signInWithRedirect(auth, provider);
-  };
+  try {
+    const result = await signInWithPopup(auth, provider);
+    console.log("LOGIN SUCCESS:", result.user.email);
+    window.location.href = "/";
+  } catch (e) {
+    console.error(e);
+    alert(e.message);
+  }
+};
 
   return (
     <div style={{ padding: 40 }}>
